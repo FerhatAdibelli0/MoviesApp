0a14a51df098b13b309a7f2eea917239d43d1015
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-17 23:21:31 +0300

    Updates

---
+++ precache-manifest.675c292c5e5d316126a72f5d584e6250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ec8cfabcd7a52654719aa06e2671c0",
+    "url": "/MoviesApp/index.html"
+  },
+  {
+    "revision": "ccc6d69472bd95b93c79",
+    "url": "/MoviesApp/static/css/main.2c0986ef.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/MoviesApp/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MoviesApp/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc6d69472bd95b93c79",
+    "url": "/MoviesApp/static/js/main.ffe2595b.chunk.js"
+  },
+  {
+    "revision": "f8e075df3dd80a643546",
+    "url": "/MoviesApp/static/js/runtime-main.907a6ae9.js"
+  }
+]);
